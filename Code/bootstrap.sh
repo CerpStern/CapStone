0dae15686fd056fca5231c846585fca220be9d70
Edward Brown <arch4rang4r@users.noreply.github.com>
2017-04-13 11:59:50 -0400

    Fixing bootstrap script some

--- Code/bootstrap.sh
+++ Code/bootstrap.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+function err {
+	echo "usage: $0 admin_email@organization.whatever"
+	exit 0
+}
+
+[ $# -eq 0 ] && err && exit 0
+
 pip install -r requirements.txt
 
 [ -f test.db ] && rm -f test.db
@@ -7,3 +14,4 @@ pip install -r requirements.txt
 python -c "from app import db; db.create_all()"
 sqlite3 test.db "insert into users (email) values (\"${1}\")"
 sqlite3 test.db "update users set admin=1"
+
